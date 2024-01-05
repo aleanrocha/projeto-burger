b2fc80573ea426fe58158cf1897207b996651efb
aleanrocha <alean.oficial.me@gmail.com>
2024-01-05 18:08:27 -0300

    creating route configurations

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import GlobalStyle from './styles/GlobalStyle'
-import Home from './containers/orders'
+import Home from './containers/home'
+import Orders from './containers/orders'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/projeto-burger',
+    element: <Home />
+  },
+  {
+    path: '/projeto-burger/orders',
+    element: <Orders />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <Home />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
